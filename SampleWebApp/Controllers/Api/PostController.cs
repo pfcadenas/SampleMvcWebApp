8084d2d934e709aa9d801f288d32401c5d2083fd
pfcadenas <pfcadenas@pinar.geocuba.cu>
2019-06-18 12:26:13 -0400

    Revert "Revert "adding functionality to give like a post""
    This reverts commit ce3a6c93b80b6b5482416c47cecb2b919941d6cb.

---
+++ SampleWebApp/Controllers/Api/PostController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DataLayer.DataClasses;
+using DataLayer.DataClasses.Concrete;
+using SampleWebApp.Models;
+
+namespace SampleWebApp.Controllers.Api
+{
+    public class PostController : ApiController
+    {
+        private SampleWebAppDb db;
+
+        public PostController() {
+            db = new SampleWebAppDb();
+        }
+        public void Like(PostLikeViewModel postLikeViewModel)
+        {       
+            Post post = db.Posts.SingleOrDefault(c => c.PostId == postLikeViewModel.PostId);
+            if (post == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            ApplicationUser user = db.Users.SingleOrDefault(c => c.UserName == User.Identity.Name);
+            if (user == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            post.Like.Add(user);
+           
+            db.SaveChanges();            
+        }
+    }
+}
---
+++ SampleWebApp/Models/PostLikeViewModels.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SampleWebApp.Models
+{
+    public class PostLikeViewModel
+    {
+        
+        public int PostId { get; set; }
+        public int Option { get; set; }
+        
+    }    
+}
